51405bc7f5baecb16264d296d82890f8c707dde8
Nguyen Marc <nguyen_marc@live.fr>
2025-03-07 11:17:55 +0100

    fix(ci): run in port 38080

--- testing/integration/deploy.sh
+++ testing/integration/deploy.sh
@@ -213,7 +213,7 @@ do
   for CALL in $(seq 1 2)
   do
     echo "Calling API $CALL"
-    HTTP_STATUS=$(curl -s -X GET 'http://localhost:8080/api' -H "Authorization: Bearer $ACCESS_TOKEN" -o $RESPONSE_FILE -w '%{http_code}')
+    HTTP_STATUS=$(curl -s -X GET 'http://localhost:38080/api' -H "Authorization: Bearer $ACCESS_TOKEN" -o $RESPONSE_FILE -w '%{http_code}')
     if [ "$HTTP_STATUS" != '200' ]; then
       >&2 echo "Unexpected status during API call: $HTTP_STATUS"
       ret=1
@@ -221,7 +221,7 @@ do
   done
 
   echo "Calling API 3"
-  HTTP_STATUS=$(curl -s -X GET 'http://localhost:8080/api' -H "Authorization: Bearer xxx" -o $RESPONSE_FILE -w '%{http_code}')
+  HTTP_STATUS=$(curl -s -X GET 'http://localhost:38080/api' -H "Authorization: Bearer xxx" -o $RESPONSE_FILE -w '%{http_code}')
   if [ "$HTTP_STATUS" != '401' ]; then
     >&2 echo "Unexpected status during API call: $HTTP_STATUS"
     ret=1
@@ -230,7 +230,7 @@ do
   for CALL in $(seq 4 5)
   do
     echo "Calling API $CALL"
-    HTTP_STATUS=$(curl -s -X GET 'http://localhost:8080/api' -H "Authorization: Bearer $ACCESS_TOKEN" -o $RESPONSE_FILE -w '%{http_code}')
+    HTTP_STATUS=$(curl -s -X GET 'http://localhost:38080/api' -H "Authorization: Bearer $ACCESS_TOKEN" -o $RESPONSE_FILE -w '%{http_code}')
     if [ "$HTTP_STATUS" != '200' ]; then
       >&2 echo "Unexpected status during API call: $HTTP_STATUS"
       ret=1
@@ -243,7 +243,7 @@ do
   for CALL in $(seq 1 2)
   do
     echo "Calling API File-based $CALL"
-    HTTP_STATUS=$(curl -s -X GET 'http://localhost:8080/api-file' -H "Authorization: Bearer $ACCESS_TOKEN" -o $RESPONSE_FILE -w '%{http_code}')
+    HTTP_STATUS=$(curl -s -X GET 'http://localhost:38080/api-file' -H "Authorization: Bearer $ACCESS_TOKEN" -o $RESPONSE_FILE -w '%{http_code}')
     if [ "$HTTP_STATUS" != '200' ]; then
       >&2 echo "Unexpected status during API call: $HTTP_STATUS"
       ret=1
@@ -251,7 +251,7 @@ do
   done
 
   echo "Calling API File-based 3"
-  HTTP_STATUS=$(curl -s -X GET 'http://localhost:8080/api-file' -H "Authorization: Bearer xxx" -o $RESPONSE_FILE -w '%{http_code}')
+  HTTP_STATUS=$(curl -s -X GET 'http://localhost:38080/api-file' -H "Authorization: Bearer xxx" -o $RESPONSE_FILE -w '%{http_code}')
   if [ "$HTTP_STATUS" != '401' ]; then
     >&2 echo "Unexpected status during API call: $HTTP_STATUS"
     ret=1
@@ -260,7 +260,7 @@ do
   for CALL in $(seq 4 5)
   do
     echo "Calling API File-based $CALL"
-    HTTP_STATUS=$(curl -s -X GET 'http://localhost:8080/api-file' -H "Authorization: Bearer $ACCESS_TOKEN" -o $RESPONSE_FILE -w '%{http_code}')
+    HTTP_STATUS=$(curl -s -X GET 'http://localhost:38080/api-file' -H "Authorization: Bearer $ACCESS_TOKEN" -o $RESPONSE_FILE -w '%{http_code}')
     if [ "$HTTP_STATUS" != '200' ]; then
       >&2 echo "Unexpected status during API call: $HTTP_STATUS"
       ret=1
